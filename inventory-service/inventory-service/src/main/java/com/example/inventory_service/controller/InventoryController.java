8c6797d1681441b9ffe9a82c32dd38f8d640d5a3
Mateusz <mateusz.chojnowski92@gmail.com>
2026-03-29 23:20:47 +0200

    add docker compose and containerization

--- inventory-service/inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
+++ inventory-service/inventory-service/src/main/java/com/example/inventory_service/controller/InventoryController.java
@@ -3,7 +3,10 @@ package com.example.inventory_service.controller;
 
 import com.example.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
--- order-service/order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -3,10 +3,11 @@ package com.example.order_service.controller;
 import com.example.order_service.dto.OrderRequest;
 import com.example.order_service.model.Order;
 import com.example.order_service.service.OrderService;
-import org.springframework.web.bind.annotation.*;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
